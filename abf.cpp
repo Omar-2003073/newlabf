e13bc335245d90de283fcdc4a16868f97eb4fb6a
Omar-2003073 <mdomarfaruk6797@gmail.com>
2025-04-19 15:08:24 +0600

    2nd commit

--- abf.cpp
+++ abf.cpp
@@ -47,3 +47,62 @@ public:
         return "WindowsScrollbar";
     }
 };
+
+// Main program (compiled when RUN_TESTS is not defined)
+#ifndef RUN_TESTS
+void createLinuxGUI() {
+    Window* window = new LinuxWindow();
+    Scrollbar* scrollbar = new LinuxScrollbar();
+    std::cout << "Created " << window->getType() << " with " << scrollbar->getType() << std::endl;
+    delete window;
+    delete scrollbar;
+}
+
+void createWindowsGUI() {
+    Window* window = new WindowsWindow();
+    Scrollbar* scrollbar = new WindowsScrollbar();
+    std::cout << "Created " << window->getType() << " with " << scrollbar->getType() << std::endl;
+    delete window;
+    delete scrollbar;
+}
+
+int main() {
+    std::cout << "Creating Linux GUI:\n";
+    createLinuxGUI();
+
+    std::cout << "Creating Windows GUI:\n";
+    createWindowsGUI();
+
+    return 0;
+}
+#endif
+
+// GTest cases (compiled when RUN_TESTS is defined)
+#ifdef RUN_TESTS
+TEST(GUITest, LinuxWindowAndScrollbar) {
+    Window* window = new LinuxWindow();
+    Scrollbar* scrollbar = new LinuxScrollbar();
+
+    EXPECT_EQ(window->getType(), "LinuxWindow");
+    EXPECT_EQ(scrollbar->getType(), "LinuxScrollbar");
+
+    delete window;
+    delete scrollbar;
+}
+
+TEST(GUITest, WindowsWindowAndScrollbar) {
+    Window* window = new WindowsWindow();
+    Scrollbar* scrollbar = new WindowsScrollbar();
+
+    EXPECT_EQ(window->getType(), "WindowsWindow");
+    EXPECT_EQ(scrollbar->getType(), "WindowsScrollbar");
+
+    delete window;
+    delete scrollbar;
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#endif
